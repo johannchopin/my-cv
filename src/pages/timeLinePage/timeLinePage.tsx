088f8a33e90b8461a09db4af4496c102cf6e79d8
johannchopin <johannchopin@pm>
2019-09-26 12:15:00 +0200

    Update timeLinePage

--- src/pages/timeLinePage/timeLinePage.tsx
+++ src/pages/timeLinePage/timeLinePage.tsx
@@ -97,32 +97,16 @@ export default class TimeLinePage extends React.Component<Props, State> {
                 <h1 className="animate-me">Mon parcours:</h1>
 
                 <div id="timeline-content">
-                    <h1>Timeline</h1>
-
                     <ul className="timeline">
-                        <li className="event animate-me" data-date="65Million B.C.">
+                        <li className="event animate-me animation-goUp">
+                            <p className="event-date">65Million B.C.</p>
                             <h3>Dinosaurs Roamed the Earth</h3>
                             <p>RAWWWWWWRRR 🐢🦂</p>
                         </li>
-                        <li className="event animate-me" data-date="2005">
+                        <li className="event animate-me animation-goUp">
+                            <p className="event-date">2005</p>
                             <h3>Creative Component Launched</h3>
                             <p>"We can be all things to all people!" 📣</p>
-                        </li>
-                        <li className="event animate-me" id="date" data-date="2009">
-                            <h3>Squareflair was Born</h3>
-                            <p>"We can be all things to Squarespace users!" 📣</p>
-                        </li>
-
-                        <li className="event animate-me" data-date="November 15, 2018">
-
-                            <h3>Squareflair Today</h3>
-
-                            <p>"We design and build from scratch!" 📣</p>
-                            <p>When we say <em><strong>100% custom</strong></em> we mean it— and we build all sites on the Squarespace Developer platform.</p>
-                            <p>Did you know that all of our pixels are hand-forged from the rarest of subpixels grown and harvested in the <em>Nerd Forest</em>? <br />🤜💥🤛</p>
-
-                            <p><strong>Our success can be measured by lives and brands enhanced by 9+ years of 100% Squarespace-focused service!</strong></p>
-
                         </li>
                     </ul>
                 </div>
