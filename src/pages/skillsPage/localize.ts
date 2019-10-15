bbe23f522592e169104844ca49075b83ca90d34d
johannchopin <johannchopin@pm>
2019-10-15 16:34:16 +0200

    Add localize in skillsPage

---
+++ src/pages/skillsPage/localize.ts
@@ -0,0 +1,13 @@
+const LOCALIZE = {
+    'de': {
+        'languages': 'Sprachen',
+    },
+    'fr': {
+        'languages': 'Langues',
+    },
+    'en': {
+        'languages': 'Languages',
+    }
+}
+
+export default LOCALIZE;
--- src/pages/skillsPage/skillsPage.tsx
+++ src/pages/skillsPage/skillsPage.tsx
@@ -4,6 +4,10 @@ import * as React from 'react';
 import './skillsPage.scss';
 // END IMPORT STYLES ZONE
 
+// IMPORT LOCALIZE ZONE
+import LOCALIZE from './localize'
+// END IMPORT LOCALIZE ZONE
+
 // IMPORT PAGEBASE ZONE
 import _pageBase from '../pageBase';
 const PageBase = new _pageBase();
@@ -30,13 +34,13 @@ const Helper = new _Helper();
 // END INIT HELPERS METHODS ZONE
 
 // IMPORT INTERFACE ZONE
-import {
-} from '../../commonInterface';
+import { TLanguages } from '../../commonInterface';
 // END IMPORT INTERFACE ZONE
 
 
 interface Props {
     currentPageIndex: number,
+    language: TLanguages,
 }
 
 interface State {
@@ -116,12 +120,14 @@ export default class SkillsPage extends React.Component<Props, State> {
     }
 
     render() {
+        const localize = LOCALIZE[this.props.language];
+
         return (
             <div id={this.state.pageId} className="swiper-slide">
                 <h1 className="animate-me slide-title">COMPÉTENCES:</h1>
                 <div className="skills-ctn">
                     <div className="langues animate-me" id="langues">
-                        <h2>LANGUES</h2>
+                        <h2>{localize.languages}</h2>
                         <div>
                             <img src={b2} />
                             <p>Allemand</p>
