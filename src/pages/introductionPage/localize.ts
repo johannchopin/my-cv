6de95e133775a65acdddf1c4bfe523e037f1dff4
johannchopin <johannchopin@pm>
2019-10-28 11:59:56 +0100

    Update translation in introductionPage localize

--- src/pages/introductionPage/localize.ts
+++ src/pages/introductionPage/localize.ts
@@ -1,6 +1,6 @@
 const LOCALIZE = {
     de: {
-        introduction_text: "\"Als 20-jähriger Student am Institut Supérieur Franco-German de Techniques, d'Économies et de Sciences (ISFATES) interessiere ich mich seit einigen Jahren für Computer und das Internet. Ehrgeizig und bereit, hoffe ich, dem Profil zu entsprechen, das Sie suchen.\"",
+        introduction_text: "\"Als 20-jähriger Student am Institut Supérieur Franco-Allemand de Techniques, d'Économies et de Sciences (ISFATES) interessiere ich mich seit einigen Jahren für Computer und das Internet. Ehrgeizig und bereit, hoffe ich, dem Profil zu entsprechen, das Sie suchen.\"",
         swipe_indication: "Wischen Sie nach rechts, um meinen Lebenslauf zu durchsuchen.",
     },
     fr: {
@@ -8,7 +8,7 @@ const LOCALIZE = {
         swipe_indication: "Balayez vers la droite pour parcourir mon CV",
     },
     en: {
-        introduction_text: "\"As a 20-year - old student at the Institut Supérieur Franco-German de Techniques, d'Économies et de Sciences (ISFATES), I have been passionate about computers and the web for several years now. Ambitious and willing, I hope to fit the profile you're looking for.\"",
+        introduction_text: "\"As a 20-year - old student at the Institut Supérieur Franco-Allemand de Techniques, d'Économies et de Sciences (ISFATES), I have been passionate about computers and the web for several years now. Ambitious and willing, I hope to fit the profile you're looking for.\"",
         swipe_indication: "Swipe to the right to browse my CV",
     }
 }
