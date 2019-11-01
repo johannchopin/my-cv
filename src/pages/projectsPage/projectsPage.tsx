bda25661684a9dc73613f4f3a6380014eb1bbe81
johannchopin <johannchopin@pm>
2019-11-01 16:38:14 +0100

    Add astropulse project

--- src/pages/projectsPage/projectsPage.tsx
+++ src/pages/projectsPage/projectsPage.tsx
@@ -20,6 +20,8 @@ import WalkinloveLogo from '../../assets/img/walkinlove-logo.png';
 import CaretakerIcon from '../../assets/img/caretaker-icon.png';
 //@ts-ignore
 import AdventskalendarIcon from '../../assets/img/adventskalendar-icon.png';
+//@ts-ignore
+import AstropulseIcon from '../../assets/img/astropulse-icon.png';
 // END IMPORT IMAGES ZONE
 
 // IMPORT COMPONENTS ZONE
@@ -117,6 +119,15 @@ export default class ProjectsPage extends React.Component<Props, State> {
                         image={AdventskalendarIcon}
                         summary="This is my L3 big project at ISFATES. We have to create a smarthome that is connected to an app. I was the project manager and and the IT manager of the project."
                         technologiesUsed={["Typescript", "React Framework", "Scss", "PhP", "Python"]}
+                    />
+                    <ProjectContainer
+                        link="https://astropulse.johannchopin.fr/"
+                        title="Astropulse-Experience"
+                        year={2018}
+                        image={AstropulseIcon}
+                        summary="My first completed PHP website create at ISFATES"
+                        linkToGitRepo="https://gitlab.com/johannchopin/htw18-19_uxdesign-form"
+                        technologiesUsed={['php', 'html', 'css', 'javascript', 'mysql']}
                     />
                     <ProjectContainer
                         link="https://test.johannchopin.fr/walkinlove/"
