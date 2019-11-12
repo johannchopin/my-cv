e91f2cccdb15c213856d5d55f8f6d5a81b88990a
johannchopin <johannchopin@pm.me>
2019-11-12 18:28:57 +0100

    Setup `app-settings` logic

---
+++ src/appSettings.ts
@@ -0,0 +1,19 @@
+// IMPORT APP SETTINGS ZONE
+import * as SETTINGS from '../app-settings.json';
+// END IMPORT APP SETTINGS ZONE
+
+// INTERFACE ZONE
+export interface IAppSettings {
+    name: string,
+    version: string,
+    api_to_use: string,
+    api: {
+        local: string
+        server: string
+    }
+}
+// END INTERFACE ZONE
+
+export default class AppSettings {
+    public static settings: IAppSettings = SETTINGS;
+}
