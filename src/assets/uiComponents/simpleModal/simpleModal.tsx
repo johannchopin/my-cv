b67fccef2edf1f4f9d216812ada982ca30c93622
johannchopin <johannchopin@pm.me>
2020-08-23 16:06:07 +0200

    ♻️ SimpleModal

--- src/assets/uiComponents/simpleModal/simpleModal.tsx
+++ src/assets/uiComponents/simpleModal/simpleModal.tsx
@@ -5,34 +5,27 @@ import './simpleModal.scss';
 // END IMPORT STYLES ZONE
 
 // IMPORT INTERFACE ZONE
-import { ISimpleModalParams } from '../../../commonInterface';
+import { SimpleModalParams } from '../../../commonInterface';
 // END IMPORT INTERFACE ZONE
 
 // INTERFACE ZONE
-interface IProps {
-    params: ISimpleModalParams,
+export interface SimpleModalProps {
+    params: SimpleModalParams,
 }
-
-interface IState { }
 // END INTERFACE ZONE
 
-export default class SimpleModal extends React.Component<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
+const SimpleModal: React.FC<SimpleModalProps> = (props) => {
+    const {type, message} = props.params
 
-        this.state = {};
-    }
-
-
-    render() {
-        return (
-            <div className="modal fade" id="simpleModal" role="dialog" aria-labelledby="alertModalLabel" aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className={'alert alert-' + this.props.params.type} role="alert">
-                        {this.props.params.message}
-                    </div>
+    return (
+        <div className="modal fade" id="simpleModal" role="dialog" aria-labelledby="alertModalLabel" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+                <div className={`alert alert-${type}`} role="alert">
+                    {message}
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
+export default SimpleModal
