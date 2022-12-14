56437cdb85adeaf2bb48f22bd6a39d053c9d3c31
johannchopin <johannchopin@pm.me>
2022-12-14 23:48:32 +0100

    ✨ Use SO-readme-profile to display tag ranking

--- src/components/TopSoAnswerer/TopSoAnswerer.tsx
+++ src/components/TopSoAnswerer/TopSoAnswerer.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react'
 
-import Icon from '~components/Icon/Icon'
-
 // IMPORT STYLES ZONE
 import './TopSoAnswerer.scss'
 // END IMPORT STYLES ZONE
@@ -9,24 +7,17 @@ import './TopSoAnswerer.scss'
 // INTERFACE ZONE
 export interface TopSoAnswererProps {
   tag: string
-  percentage: number
 }
 // END INTERFACE ZONE
 
-// IMPORT LOCALIZE ZONE
-import * as localize from './localize.json'
-import Localize from '~Localize'
-// END IMPORT LOCALIZE ZONE
+const USER_ID = 8583669
 
 const TopSoAnswerer: React.FC<TopSoAnswererProps> = (props) => {
-  const { tag, percentage } = props
-
-  const linkToProfile = `https://stackoverflow-readme-profile.vercel.app/tags-league/${tag}/users/8583669`
+  const { tag } = props
 
   return (
-    <a href={linkToProfile} target="_blank" className="top-so-answerer mt-1">
-      <Icon icon="external-link-alt" className="pr-1" />
-      <Localize translations={localize.top_answerers} vars={{ __PERCENTAGE__: percentage }} />
+    <a href={`https://stackoverflow-readme-profile.vercel.app/tags-league/${tag}/users/${USER_ID}`} className="mt-1">
+      <img src={`https://stackoverflow-readme-profile.johannchopin.fr/tags-league-ranking/${tag}/${USER_ID}?theme=default`} alt={`my stackoverflow ranking for ${tag}`}/>
     </a>
   )
 }
